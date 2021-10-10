9870435d97a509541d140d27dd6181ab11173938
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2021-10-10 01:46:45 -0300

    mostrar numero digitado

--- mostrarnumero.py
+++ mostrarnumero.py
@@ -1,2 +1,6 @@
-#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+#Faça um Programa que peça um número e então mostre a mensagem 
+# O número informado foi [número].
+
+numero = input('Digite um numero') 
+print('O numero digitado foi' + numero)
 
